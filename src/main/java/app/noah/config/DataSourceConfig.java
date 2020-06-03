19e72a6d84ec26b32ba61f9f08cdb013ef1f810d
yky1990@glowmee.com <yky1990@glowmee.com>
2020-06-03 16:08:18 +0900

    mysql 개통된 것 확인 + mysql 개발기 정보 은닉

--- src/main/java/app/noah/config/DataSourceConfig.java
+++ src/main/java/app/noah/config/DataSourceConfig.java
@@ -1,42 +1,39 @@
-//package app.noah.config;
-//
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.context.annotation.PropertySource;
-//import org.springframework.jdbc.datasource.DriverManagerDataSource;
-//
-//import javax.sql.DataSource;
-//
-//@Configuration
-//@PropertySource(value = {"classpath:${spring.profiles.active}/application.properties"})
-//public class DataSourceConfig
-//{
-//    @Value("${spring.datasource.driver-class-name}")
-//    private String driverName;
-//
-//    @Value("${spring.datasource.url}")
-//    private String url;
-//
-//    @Value("${spring.datasource.username}")
-//    private String username;
-//
-//    @Value("${spring.datasource.password}")
-//    private String password;
-//
-//
-//    @Bean
-//    public DataSource dataSource() {
-//        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-//
-//        System.out.println("driverName:"+driverName);
-//        System.out.println("url:"+url);
-//        System.out.println("username:"+username);
-//        dataSource.setDriverClassName(driverName);
-//        dataSource.setUrl(url);
-//        dataSource.setUsername(username);
-//        dataSource.setPassword(password);
-//
-//        return dataSource;
-//    }
-//}
+package app.noah.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+@PropertySource(value = {"classpath:${spring.profiles.active}/application.properties"})
+public class DataSourceConfig
+{
+    @Value("${spring.datasource.driver-class-name}")
+    private String driverName;
+
+    @Value("${spring.datasource.url}")
+    private String url;
+
+    @Value("${spring.datasource.username}")
+    private String username;
+
+    @Value("${spring.datasource.password}")
+    private String password;
+
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+
+        dataSource.setDriverClassName(driverName);
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+
+        return dataSource;
+    }
+}
